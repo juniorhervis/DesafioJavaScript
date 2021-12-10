661f50c11182cdbb1a07d8bbbb1524e7cc4ff5e5
Edson Luiz Hervis Junior <juniorhervis1@gmail.com>
2021-12-10 11:12:29 -0300

    melhorias no código

--- DesafioJsTIacademy/script/cep.js
+++ DesafioJsTIacademy/script/cep.js
@@ -1,27 +1,36 @@
 window.onload = () => {
   const btnBuscar = document.querySelector("#buscarDados");
-  const cep_ = document.querySelector("#cep")
-  try {
-    btnBuscar.addEventListener("click", () => {
-    cep_.value.replace("-","")
-      if(cep_.value.length == 8 )
-        dadosCep(cep_.value); 
-    })
-  } catch (error) {
-    alert("Digite um cep válido");
-  }
+  const cep_ = document.querySelector("#CEP");
+
+  btnBuscar.addEventListener("click", () => {
+    if (cep_.value.length !== 8) {
+      alert("O CEP precisa ter 8 dígitos!");
+    }
+    dadosCep(cep_.value);
+    btnBuscar.classList.add("disabled");
+  });
 
   let dadosCep = async function (cep) {
     let url = `https://viacep.com.br/ws/${cep}/json/`;
     let dadosFetch = await fetch(url);
     let dadosJson = await dadosFetch.json();
-    resultadoCep(dadosJson)
+    if (dadosJson.hasOwnProperty("erro")) {
+      alert("CEP não encontrado!");
+    } else {
+      resultadoCep(dadosJson);
+    }
   };
 
-function resultadoCep(dadosCep) {
-    for (let campo in dadosCep) {
+  function resultadoCep(cepDados) {
+    // document.querySelector("#logradouro").value = cepDados.logradouro;
+    // document.querySelector("#bairro").value = cepDados.bairro;
+    // document.querySelector("#localidade").value = cepDados.localidade;
+    // document.querySelector("#uf").value = cepDados.uf;
+
+    for (let campo in cepDados) {
       if (document.querySelector(`#${campo}`)) {
-        document.querySelector(`#${campo}`).value = dadosCep[campo];
+        document.querySelector(`#${campo}`).value = cepDados[campo];
+        console.log(campo);
       }
     }
   }
